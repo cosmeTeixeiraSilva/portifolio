ed4403b91ef7fbc7604d1552d2965f59daf79799
Eng Cosme  - Docente Senac/MG <71655160+cosmeTeixeiraSilva@users.noreply.github.com>
2025-02-24 12:12:31 -0300

    status de envio

--- app/_components/formemail.jsx
+++ app/_components/formemail.jsx
@@ -4,83 +4,91 @@ import { sendEmail } from "../actions/actions";
 
 export default function ContactForm() {
   const [status, setStatus] = useState("");
-  //useEffect monitorando alteração de state da variavel status
+
   useEffect(() => {
     if (status) {
       const timer = setTimeout(() => {
-        setStatus(""); // Remove a mensagem após 3 segundos
-      }, 3000);
+        setStatus(""); // Remove a mensagem após 3.5 segundos
+      }, 3500);
       return () => clearTimeout(timer); // Limpa o timeout se o componente for desmontado
     }
   }, [status]);
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setStatus("Aguarde por favor, enviando e-mail...");
 
     const form = new FormData(event.target);
-    //form.append("phone", phone); // Adiciona o telefone manualmente
-
     const response = await sendEmail(form);
+
     setStatus(response.message);
+
+    if (response.success) {
+      event.target.reset();
+    }
   }
 
   return (
-    <form
-      id="emailForm"
-      onSubmit={handleSubmit}
-      className="flex flex-col gap-4 w-full mx-auto p-4 rounded"
-    >
-      <p className="text-center font-semibold underline underline-offset-4">
-        Entre em Contato:
-      </p>
-
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Seu e-mail"
-        required
-        className="border p-2 rounded w-full sm:w-2/3 mx-auto text-black"
-      />
-
-      <input
-        type="text"
-        id="subject"
-        name="subject"
-        placeholder="Assunto"
-        required
-        className="border p-2 rounded w-full sm:w-2/3 mx-auto text-black"
-      />
-
-      {/* Campo com máscara de telefone */}
-      <input
-        type="number"
-        name="celular"
-        id="celular"
-        max={999999999999}
-        placeholder="35900000000"
-        required
-        className="border p-2 rounded w-full sm:w-2/3 mx-auto text-black"
-      />
-
-      <textarea
-        name="message"
-        id="message"
-        placeholder="Mensagem"
-        required
-        className="border p-2 rounded w-full sm:w-2/3 mx-auto text-black"
-      />
+    <div className="relative w-full sm:w-2/3 mx-auto mt-10">
+      {/* Mensagem de Status - Sobrepondo o formulário */}
       {status && (
-        <p className=" bg-green-400 p-2 rounded border-2 border-white font-semibold w-full sm:w-2/3 sm:text-md mx-auto m-2 text-sm">
+        <p className="absolute top-2 left-1/2 transform -translate-x-1/2 bg-green-400 text-white p-2 rounded border-2 border-white font-semibold text-center shadow-md z-10 w-3/4">
           {status}
         </p>
       )}
-      <button
-        type="submit"
-        className="bg-blue-500 text-white p-2 rounded w-full sm:w-2/3 mx-auto"
+
+      <form
+        id="emailForm"
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 p-4 rounded bg-white relative z-0"
       >
-        Enviar
-      </button>
-    </form>
+        <p className="text-center font-semibold underline underline-offset-4 text-black">
+          Entre em Contato:
+        </p>
+
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Seu e-mail"
+          required
+          className="border p-2 rounded w-full text-black"
+        />
+
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          placeholder="Assunto"
+          required
+          className="border p-2 rounded w-full text-black"
+        />
+
+        <input
+          type="number"
+          name="celular"
+          id="celular"
+          max={999999999999}
+          placeholder="35900000000"
+          required
+          className="border p-2 rounded w-full text-black"
+        />
+
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Mensagem"
+          required
+          className="border p-2 rounded w-full text-black"
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded w-full"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
   );
 }
---
+++ app/actions/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div>
+      <h1>Enviando E-mail....</h1>
+    </div>
+  );
+}
